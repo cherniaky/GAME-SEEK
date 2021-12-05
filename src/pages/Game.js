4b84186fc8390c8b79125e4aa67cde44f80918f0
Yurii <juracherniak@gmail.com>
2021-12-05 13:52:40 +0100

    add game mechanics

--- src/pages/Game.js
+++ src/pages/Game.js
@@ -1,6 +1,9 @@
 import React, { useEffect, useState } from "react";
-import allLevelData from '../allLevelData';
-
+import allLevelData from "../allLevelData";
+import OutsideClickHandler from "react-outside-click-handler";
+import '../styles/Game.css';
+import CharacterDropdown from "../components/CharacterDropdown";
+import Modal from "../components/Modal";
 
 export default function Game({
     level,
@@ -29,11 +32,132 @@ export default function Game({
     setInHome(false);
     setInInfo(false);
 
-     useEffect(() => {
-         let loadedCharacters = allLevelData[level].characters;
-         setImage(allLevelData[level].board);
-         setCharacters(loadedCharacters);
-     }, []);
+    useEffect(() => {
+        let loadedCharacters = allLevelData[level].characters;
+        setImage(allLevelData[level].board);
+        setCharacters(loadedCharacters);
+    }, []);
 
-    return <div></div>;
+    useEffect(() => {
+        setGameover(characters.every((char) => char.found));
+    }, [characters]);
+
+    useEffect(() => {
+        if (gameover === true) {
+            let endingTimestamp = Date.now();
+            let score = (endingTimestamp - startTime) / 1000;
+            let date = new Date().toString().split(" ").splice(1, 3).join(" ");
+            if (score < 0.1) {
+                return;
+            }
+            // firestore
+            //     .collection("games")
+            //     .add({
+            //         startTime: startTime,
+            //         endTime: endingTimestamp,
+            //         level,
+            //         characters: characters,
+            //         elapsedSeconds: score,
+            //         date: date,
+            //     })
+            //     .then((docRef) => {
+            //         setgameId(docRef.id);
+            //         firestore
+            //             .collection("games")
+            //             .doc(docRef.id)
+            //             .onSnapshot((doc) => {
+            //                 const data = doc.data();
+            //                 setElapsedSeconds(data?.elapsedSeconds);
+            //             });
+            //     });
+             setElapsedSeconds(score);
+        }
+    }, [gameover]);
+
+    const getLocationImageClick = (e) => {
+        const xCoord = Math.round(
+            (e.nativeEvent.offsetX / e.nativeEvent.target.offsetWidth) * 100
+        );
+        const yCoord = Math.round(
+            (e.nativeEvent.offsetY / e.nativeEvent.target.offsetHeight) * 100
+        );
+        const coords = { xCoord, yCoord };
+        return coords;
+    };
+
+    const updateClickLocation = (coords) => {
+        const { xCoord, yCoord } = coords;
+        const updatedCoords = { left: xCoord + "%", top: yCoord + "%" };
+        setClickLocation(updatedCoords);
+        setShowDropdown(true);
+    };
+
+    const imageClick = (e) => {
+        const coords = getLocationImageClick(e);
+        setCoords(coords);
+        updateClickLocation(coords);
+    };
+
+    const hideDropdown = () => setShowDropdown(false);
+
+    const isCoordWithinTwoDegrees = (coord1, coord2) => {
+        return (
+            coord1 === coord2 ||
+            coord1 + 1 === coord2 ||
+            coord1 + 2 === coord2 ||
+            coord1 - 1 === coord2 ||
+            coord1 - 2 === coord2
+        );
+    };
+
+    const dropdownClick = (character) => {
+        //const gameSelection = { coords, character, gameId, level };
+
+        // check for character
+        const selectedCharacter = characters.find((char) => {
+            return char.name === character;
+        });
+        const { xCoord, yCoord } = selectedCharacter;
+        const isCharacterAtCoords =
+            isCoordWithinTwoDegrees(xCoord, coords.xCoord) &&
+            isCoordWithinTwoDegrees(yCoord, coords.yCoord);
+        if (isCharacterAtCoords) {
+            const updatedCharacters = characters.map((char) =>
+                char.name === character ? { ...char, found: true } : char
+            );
+            setCharacters(updatedCharacters);
+        }
+
+        
+        //firestore.collection("playerSelection").add(gameSelection);
+        hideDropdown();
+    };
+
+    return (
+        <div className="game-wrapper">
+            <div className="relative">
+                <OutsideClickHandler onOutsideClick={hideDropdown}>
+                    <img
+                        className="game-board-image"
+                        src={image}
+                        alt="Game Level"
+                        onClick={imageClick}
+                    />
+                    <CharacterDropdown
+                        show={showDropdown}
+                        characters={characters}
+                        clickLocation={clickLocation}
+                        clicked={dropdownClick}
+                    />
+                </OutsideClickHandler>
+            </div>
+            <Modal
+                showModal={gameover}
+                seconds={elapsedSeconds}
+                username={username}
+                updateUsername={updateUsername}
+                submitScore={()=>console.log(7)}
+            />
+        </div>
+    );
 }
